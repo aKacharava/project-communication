c2617ccfa01ba4ead4d5704430c16c2e5e9c581c
Michael <m.malakoff1998@gmail.com>
2020-09-03 22:03:11 +0200

    NPC Wandering, Aggro, Collisions
    Npc wandering, aggro on player and pathfinding (using navmesh) implemented. Also normilized player movement.

---
+++ shooter-corona/Assets/scripts/NpcController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcController : MonoBehaviour
+{
+    
+    public float lookRadius = 10f;
+    public float wanderRadius;
+    public float wanderTimer;
+
+    private float timer;
+    private bool attacking;
+    private Transform target;
+    private NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
+        attacking = false;
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        if (distance <= lookRadius)
+        {
+            agent.SetDestination(target.position);
+            attacking = true;
+
+            if (distance <= agent.stoppingDistance)
+            {
+                //attack target
+                //face target
+                FaceTarget();
+            }
+        }
+        else attacking = false;
+        if (timer >= wanderTimer && !attacking)
+        {
+            Vector3 newPosition = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPosition);
+            timer = 0;
+        }
+    }
+
+    private void FaceTarget ()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float distance, int layermask)
+    {
+        Vector3 randomDirection = UnityEngine.Random.insideUnitSphere * distance;
+
+        randomDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randomDirection, out navHit, distance, layermask);
+
+        return navHit.position;
+    }
+}
