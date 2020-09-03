8ccba7a2b606c9dc554348ed0ae0150d98d37c47
Alexander Kacharava <a.kacharava94@gmail.com>
2020-09-03 11:16:28 +0200

    Movement, Aiming, terrain, Camera
    Movement & Aiming implemented, Changed the terrain, Changed the camera behaviou

---
+++ shooter-corona/Assets/scripts/PlayerAim.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    private Camera mainCam;
+
+    Vector3 lookPos;
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 mousePos = Input.mousePosition;
+        //Vector3 playerPos = mainCam.WorldToScreenPoint(transform.localPosition);
+
+        //Vector2 offset = new Vector3(mousePos.x - playerPos.x, mousePos.y - playerPos.z);
+
+        //float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg - 90;
+
+        //transform.rotation = Quaternion.Euler(0f, -angle, 0f);
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            lookPos = hit.point;
+        }
+
+        Vector3 lookDir = lookPos - transform.position;
+        lookDir.y = 0;
+
+        transform.LookAt(transform.position + lookDir, Vector3.up);
+    }
+}
---
+++ shooter-corona/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody rigBody;
+
+    Vector3 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        /// Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.z = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        /// Movement 
+        rigBody.MovePosition(rigBody.position + movement * moveSpeed * Time.deltaTime);
+    }
+}
