e76dbdfad1d30be3f08d87529e5bed4d5728299e
Michael <m.malakoff1998@gmail.com>
2020-09-10 00:37:45 +0200

    Healthbar
    Added health bar (Health changes on Input.GetKeyDown(KeyCode.Space))

---
+++ shooter-corona/Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHealth = 100.0f;
+    public float minHealth = 0.0f;
+    public float currentHealth;
+    public float healthbarTestValue = 10.0f;
+
+    public Healthbar playerHealthbar;
+    void Start()
+    {
+        currentHealth = minHealth;
+        playerHealthbar.SetMinHealth(minHealth);
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && currentHealth < maxHealth)
+        {
+            TakeDamage(healthbarTestValue);
+        }
+    }
+
+    void TakeDamage(float damage)
+    {
+        currentHealth += damage;
+        playerHealthbar.SetHealth(currentHealth);
+    }
+}
