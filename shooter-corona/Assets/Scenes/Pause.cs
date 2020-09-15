0218e488fbdddb4da2252611bace82ca97042af8
andrei1200 <bugan_andrei@yahoo.com>
2020-09-15 18:17:36 +0300

    Finished Menu settings
    Implemented the feedback

---
+++ shooter-corona/Assets/Scenes/Pause.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private string menu_scene = "Menu";
+
+    public Transform pauseScreen;
+
+    private bool paused = false;
+    private bool isMenuOpen = true;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        pauseScreen.gameObject.SetActive(false);
+        paused = true; Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape)&&!isMenuOpen)
+        {
+            paused = !paused;
+            pauseScreen.gameObject.SetActive(paused);
+            Time.timeScale = paused ? 0 : 1;
+        }
+    }
+
+    public void Continue()
+    {
+        if (paused)
+        {
+            paused = false;
+            pauseScreen.gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+    public void CloseHelpMenu()
+    {
+        isMenuOpen = false;
+    }
+
+    public void ReturnToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(menu_scene);
+    }
+}
---
+++ shooter-corona/Assets/Scenes/Settings.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public Transform musicContainer;
+    public Transform soundsContainer;
+
+    public Transform brightnessPanel;
+
+    public Slider brightnessSlider;
+    public Slider musicSlider;
+    public Slider soundsSlider;
+
+    private CanvasGroup brightness;
+
+    private void Start()
+    {
+        brightness = brightnessPanel.GetComponent<CanvasGroup>();
+
+        brightness.alpha = (1f - brightnessSlider.value) / 2;
+
+        UpdateSounds(musicContainer, musicSlider);
+
+        UpdateSounds(soundsContainer, soundsSlider);
+    }
+
+    private void UpdateSounds(Transform container, Slider slider)
+    {
+        AudioSource[] audios = container.GetComponentsInChildren<AudioSource>();
+
+        foreach (AudioSource audio in audios)
+        {
+            audio.volume = slider.value;
+        }
+    }
+
+    public void SetMusicVolume(Slider slider)
+    {
+        UpdateSounds(musicContainer, slider);
+    }
+
+    public void SetSoundsVolume(Slider slider)
+    {
+        UpdateSounds(soundsContainer, slider);
+    }
+
+    public void SetBrightness(Slider slider)
+    {
+        brightness.alpha = (1f - slider.value) / 2;
+    }
+}
