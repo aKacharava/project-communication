71f5b29d4d0761a1413381c43db89fb2ce1c4d09
Alexander Kacharava <a.kacharava94@gmail.com>
2020-09-15 23:40:02 +0200

    repositioning of npcs, baked environment

--- shooter-corona/Assets/Scripts/NpcDialogueManager.cs
+++ shooter-corona/Assets/Scripts/NpcDialogueManager.cs
@@ -7,27 +7,27 @@ using UnityEngine;
 
 public class NpcDialogueManager : MonoBehaviour
 {
-    public TextMeshProUGUI floatingTextPrefab;
+    //public TextMeshProUGUI floatingTextPrefab;
 
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.tag == "Bullet")
-        {
-            ShowFloatingText();
-            SetFloatingText();
-        }
-    }
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Bullet")
+    //    {
+    //        ShowFloatingText();
+    //        SetFloatingText();
+    //    }
+    //}
 
-    void SetFloatingText()
-    {
-        floatingTextPrefab.text = "blablabllalabl";
-    }
+    //void SetFloatingText()
+    //{
+    //    floatingTextPrefab.text = "blablabllalabl";
+    //}
 
-    void ShowFloatingText()
-    {
-        if (floatingTextPrefab.gameObject.activeInHierarchy == false)
-        {
-            floatingTextPrefab.gameObject.SetActive(true);
-        }
-    }
+    //void ShowFloatingText()
+    //{
+    //    if (floatingTextPrefab.gameObject.activeInHierarchy == false)
+    //    {
+    //        floatingTextPrefab.gameObject.SetActive(true);
+    //    }
+    //}
 }
