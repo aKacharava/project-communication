3c8874d262bc46c8a7b5ae6cd493601d7b89fda6
Alexander Kacharava <a.kacharava94@gmail.com>
2020-09-18 01:20:26 +0200

    Updated everything

--- shooter-corona/Assets/Scripts/UIScripts/Score.cs
+++ shooter-corona/Assets/Scripts/UIScripts/Score.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     public Text textScore;
+    public GameObject victoryUI;
+    public GameObject gameUI;
 
     int npcs;
     int maskedNpcs;
@@ -15,5 +17,19 @@ public class Score : MonoBehaviour
         npcs = GameObject.FindGameObjectsWithTag("NPC").Length;
         maskedNpcs = GameObject.FindGameObjectsWithTag("NPC_Masked").Length;
         textScore.text = "Amount NPCs: " + maskedNpcs.ToString() + "/" + npcs.ToString();
+
+        ShowVictoryScreen();
+    }
+
+    void ShowVictoryScreen()
+    {
+        if (maskedNpcs == npcs)
+        {
+            SoundManager.StopSound("trainstation sound", true);
+            SoundManager.PlaySound("victory sound", true, false);
+            victoryUI.SetActive(true);
+            gameUI.SetActive(false);
+            Time.timeScale = 0f;
+        }
     }
 }
